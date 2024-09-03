e70ea24f4e428ec198c077b5e948bf8b4088644d
Subash Dev <154237809+sarkardocs@users.noreply.github.com>
2024-09-03 23:02:54 +0000

    Your commit message

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,139 +1,193 @@
 'use client';
 
-import { useEffect } from 'react';
+import React from 'react';
+import Link from '@/components/Link';
 import MainLayout from '@/layouts/MainLayout';
-import { CSSProperties } from 'react'; // Import CSSProperties for type safety
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faReact,
+  faDocker,
+  faPython,
+  faNode,
+  faGithub,
+  faJava,
+  faHtml5,
+  faWindows,
+  faApple,
+} from '@fortawesome/free-brands-svg-icons';
 
-export default function About() {
-  useEffect(() => {
-    // Dynamically load Font Awesome CSS
-    const link = document.createElement('link');
-    link.rel = 'stylesheet';
-    link.href = 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css';
-    document.head.appendChild(link);
-
-    // Load Poppins font
-    const fontLink = document.createElement('link');
-    fontLink.rel = 'stylesheet';
-    fontLink.href =
-      'https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600;700&display=swap';
-    document.head.appendChild(fontLink);
-
-    return () => {
-      document.head.removeChild(link);
-      document.head.removeChild(fontLink);
-    };
-  }, []);
-
+function Expertise() {
   return (
     <MainLayout>
-      <div style={containerStyle}>
-        <header style={headerStyle}>
-          <h1 style={titleStyle}>Hi, I’m Subash</h1>
-          <p style={subtitleStyle}>Dev</p>
-        </header>
-        <section style={sectionStyle}>
-          <h2 style={sectionTitleStyle}>About Me</h2>
-          <p style={textStyle}>
-            I am a passionate developer and UI designer with experience in creating user-friendly
-            interfaces and robust applications. I thrive in collaborative environments and enjoy
-            solving complex problems through innovative solutions.
-          </p>
-        </section>
-        <section style={sectionStyle}>
-          <h2 style={sectionTitleStyle}>Skills</h2>
-          <ul style={listStyle}>
-            <li style={listItemStyle}>Front-end Development: React, Vue.js, HTML, CSS</li>
-            <li style={listItemStyle}>UI/UX Design: Figma, Sketch, Adobe XD</li>
-            <li style={listItemStyle}>Backend Development: Node.js, Express</li>
-            <li style={listItemStyle}>Version Control: Git, GitHub</li>
-          </ul>
-        </section>
-        <section style={sectionStyle}>
-          <h2 style={sectionTitleStyle}>Projects</h2>
-          <ul style={listStyle}>
-            <li style={listItemStyle}>
-              <a
-                href="https://sarkardocs.vercel.app"
-                target="_blank"
-                rel="noopener noreferrer"
-                aria-label="SarkarDocs"
-              >
-                SarkarDocs
-              </a>
-              : Restful API docs for devs.
-            </li>
-            <li style={listItemStyle}>
-              <a
-                href="https://github.com/Flux.git"
-                target="_blank"
-                rel="noopener noreferrer"
-                aria-label="Flux Dev"
-              >
-                Flux Dev
-              </a>
-              : Schnell Image Generator API.
-            </li>
-          </ul>
-        </section>
+      <div className="container" id="expertise">
+        <div className="skills-container">
+          <h1>E X P E R T I S E</h1>
+          <div className="skills-grid">
+            <div className="skill">
+              <FontAwesomeIcon icon={faReact} size="5x" className="icon" />
+              <h3 className="title">Web Development</h3>
+              <p className="description">
+                I have built a diverse array of web applications from scratch using modern
+                technologies such as React and Flask. I have a strong proficiency in the SDLC
+                process and frontend + backend development.
+              </p>
+            </div>
+
+            <div className="skill">
+              <FontAwesomeIcon icon={faDocker} size="5x" className="icon" />
+              <h3 className="title">DevOps & Automation</h3>
+              <p className="description">
+                Once the application is built, I help clients set up DevOps testing, CI/CD
+                pipelines, and deployment automation to support the successful Go-Live.
+              </p>
+            </div>
+
+            <div className="skill">
+              <FontAwesomeIcon icon={faPython} size="5x" className="icon" />
+              <h3 className="title">GenAI & LLM</h3>
+              <p className="description">
+                Stay relevant in the market by leveraging the latest AI models in your projects. I
+                have professional experience building enterprise-grade GenAI-enabled solutions to
+                empower intelligent decision-making.
+              </p>
+            </div>
+
+            <div className="skill">
+              <FontAwesomeIcon icon={faNode} size="5x" className="icon" />
+              <h3 className="title">Node.js Backend</h3>
+              <p className="description">
+                Proficient in developing scalable server-side applications using Node.js, with a
+                focus on performance and maintainability.
+              </p>
+            </div>
+
+            <div className="skill">
+              <FontAwesomeIcon icon={faJava} size="5x" className="icon" />
+              <h3 className="title">Java Development</h3>
+              <p className="description">
+                Experience in developing robust and scalable applications using Java, with a focus
+                on enterprise-level solutions and backend systems.
+              </p>
+            </div>
+
+            <div className="skill">
+              <FontAwesomeIcon icon={faHtml5} size="5x" className="icon" />
+              <h3 className="title">HTML/CSS</h3>
+              <p className="description">
+                Experience with building semantic and accessible web pages using HTML5.
+              </p>
+            </div>
+
+            <div className="skill">
+              <FontAwesomeIcon icon={faGithub} size="5x" className="icon" />
+              <h3 className="title">Version Control</h3>
+              <p className="description">
+                Experienced with version control systems, particularly Git and GitHub, for
+                collaborative development and code management.
+              </p>
+            </div>
+
+            <div className="skill">
+              <FontAwesomeIcon icon={faApple} size="5x" className="icon" />
+              <h3 className="title">Apple Ecosystem</h3>
+              <p className="description">
+                Experienced in using and developing applications within the Apple ecosystem,
+                including macOS and iOS environments.
+              </p>
+            </div>
+
+            <div className="skill">
+              <FontAwesomeIcon icon={faWindows} size="5x" className="icon" />
+              <h3 className="title">Windows OS</h3>
+              <p className="description">
+                Proficient in using and managing Windows operating systems for various applications,
+                including software development and system administration.
+              </p>
+            </div>
+          </div>
+
+          <div className="back-to-home">
+            <Link href="/">Back to homepage</Link>
+          </div>
+        </div>
+
+        <style jsx>
+          {`
+            .skills-container {
+              padding: 5% 10%;
+              display: flex;
+              flex-direction: column;
+              text-align: center;
+            }
+
+            .skills-container h1 {
+              font-size: 2.5rem;
+              font-weight: bold;
+              margin-bottom: 2rem;
+            }
+
+            .skills-container .skills-grid {
+              display: grid;
+              grid-template-columns: repeat(3, 1fr);
+              grid-gap: 50px;
+              margin-top: 3rem;
+            }
+
+            .skills-container .skills-grid .skill {
+              align-items: center;
+              text-align: center;
+              margin-bottom: 3rem;
+            }
+
+            .icon {
+              margin-bottom: 1rem;
+            }
+
+            .title {
+              font-size: 1.5rem;
+              font-weight: bold;
+              font-family: 'Arial', sans-serif;
+              margin: 1rem 0;
+            }
+
+            .description {
+              font-size: 1.2rem;
+              font-weight: normal;
+              margin: 0;
+            }
+
+            .back-to-home {
+              text-align: center;
+              margin-top: 3rem;
+              margin-bottom: 2rem;
+            }
+
+            @media (max-width: 768px) {
+              .skills-container {
+                display: block;
+                padding-left: 5%;
+                padding-right: 5%;
+              }
+
+              .skills-container .skills-grid {
+                display: initial;
+              }
+
+              .skills-container .skills-grid .skill {
+                padding-bottom: 5%;
+              }
+            }
+
+            @media (max-width: 567px) {
+              .skills-container {
+                font-size: 12px;
+              }
+            }
+          `}
+        </style>
       </div>
     </MainLayout>
   );
 }
 
-// Styles for the portfolio page
-const containerStyle: CSSProperties = {
-  padding: '20px',
-  maxWidth: '800px',
-  margin: 'auto',
-  fontFamily: 'Poppins, sans-serif', // Use Poppins font
-};
-
-const headerStyle: CSSProperties = {
-  textAlign: 'center',
-  marginBottom: '30px', // Increased margin for spacing
-};
-
-const titleStyle: CSSProperties = {
-  fontSize: '2.5rem',
-  margin: '0 0 10px 0', // Margin adjustments for spacing
-  fontWeight: '600', // Semi-bold for a clean look
-  color: '#08a76d',
-  fontFamily: 'Poppins, sans-serif', // Use Poppins font
-};
-
-const subtitleStyle: CSSProperties = {
-  fontSize: '1.5rem',
-  fontWeight: '400',
-  color: '#555',
-  fontFamily: 'Poppins, sans-serif',
-};
-
-const sectionStyle: CSSProperties = {
-  marginBottom: '30px', // Increased margin for spacing
-};
-
-const sectionTitleStyle: CSSProperties = {
-  fontSize: '2rem',
-  fontWeight: '600',
-  color: '#fff',
-  backgroundColor: '#154c79', // Purple background for highlight
-  padding: '5px 10px', // Padding for text fitting
-  borderRadius: '5px', // Slight rounding of corners
-  display: 'inline-block', // Ensures background fits tightly around text
-  fontFamily: 'Poppins, sans-serif',
-};
-
-const textStyle: CSSProperties = {
-  fontSize: '1.2rem',
-  lineHeight: '1.6',
-};
-
-const listStyle: CSSProperties = {
-  listStyleType: 'none',
-  padding: 0,
-};
-
-const listItemStyle: CSSProperties = {
-  marginBottom: '15px', // Increased margin for spacing
-};
+export default Expertise;
