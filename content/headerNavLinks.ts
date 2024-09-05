c027c0a71b7ae5b4414c1e95f55d4b9bab603f55
Subash Dev <154237809+sarkardocs@users.noreply.github.com>
2024-09-05 11:01:01 +0000

    Initial commit

--- content/headerNavLinks.ts
+++ content/headerNavLinks.ts
@@ -2,6 +2,7 @@ const headerNavLinks = [
   { href: '/hi', title: 'Hi' },
   { href: '/docs', title: 'Docs' },
   { href: '/about', title: 'About' },
+  { href: '/project', title: 'Projects' },
 ];
 
 export default headerNavLinks;
