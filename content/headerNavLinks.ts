a6ceb6f1b272296e85fb4a697416c6bc6bdc0747
Subash Dev <154237809+sarkardocs@users.noreply.github.com>
2024-09-06 21:00:50 +0545

    Update headerNavLinks.ts

--- content/headerNavLinks.ts
+++ content/headerNavLinks.ts
@@ -1,7 +1,6 @@
 const headerNavLinks = [
   { href: '/hi', title: 'Hi' },
   { href: '/car', title: 'Car' },
-  { href: '/docs', title: 'Docs' },
   { href: '/about', title: 'About' },
   { href: '/project', title: 'Project' },
 ];
