b6db46139f587e4c9197a63bdcc6be2eb86ac0db
Subash Baniya <154237809+sarkardocs@users.noreply.github.com>
2024-09-14 17:06:33 +0545

    Update not-found.tsx

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -11,17 +11,19 @@ import SectionContainer from '@/components/SectionContainer';
 export default function FourZeroFour() {
   return (
     <SectionContainer>
-      <div className="space-x-2 pt-6 pb-8 md:space-y-5">
-        <h1 className="text-6xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 md:border-r-2 md:px-6 md:text-8xl md:leading-14">
-          404
-        </h1>
-      </div>
-      <div className="max-w-md">
-        <p className="mb-4 text-xl font-bold leading-normal md:text-2xl">Page not found!</p>
-        <p className="mb-8">
-          Page under construction! But dont worry, you can find other content on our homepage.
-        </p>
-        <Link href="/">Back to homepage </Link>
+      <div className="flex flex-col min-h-screen justify-center items-center">
+        <div className="space-x-2 pt-6 pb-8 md:space-y-5">
+          <h1 className="text-6xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 md:border-r-2 md:px-6 md:text-8xl md:leading-14">
+            404
+          </h1>
+        </div>
+        <div className="max-w-md text-center">
+          <p className="mb-4 text-xl font-bold leading-normal md:text-2xl">Page not found!</p>
+          <p className="mb-8">
+            Page under construction! But don't worry, you can find other content on our homepage.
+          </p>
+          <Link href="/">Back to homepage</Link>
+        </div>
       </div>
     </SectionContainer>
   );
