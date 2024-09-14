fc157acb1ea1123af48cef5277999b57e1f8b015
Subash Baniya <154237809+sarkardocs@users.noreply.github.com>
2024-09-14 18:35:07 +0545

    Update not-found.tsx

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,10 +1,18 @@
 import Link from '@/components/Link';
 import SectionContainer from '@/components/SectionContainer';
 
+// TODO: Comment out when https://github.com/vercel/next.js/issues/50566 is fixed.
+
+// export const metadata = {
+//   title: '404 - Subash Baniya',
+//   description: 'Not Found - Subash Baniya',
+// };
+
 export default function FourZeroFour() {
   return (
-    <div className="flex flex-col min-h-screen">
-      <main className="flex-grow">
+    <div className="min-h-screen flex flex-col justify-between">
+      {/* Main Content */}
+      <div>
         <div className="space-x-2 pt-6 pb-8 md:space-y-5">
           <h1 className="text-6xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 md:border-r-2 md:px-6 md:text-8xl md:leading-14">
             404
@@ -17,10 +25,10 @@ export default function FourZeroFour() {
           </p>
           <Link href="/">Back to homepage</Link>
         </div>
-      </main>
-      <SectionContainer>
-        {/* You can put any footer content here if needed */}
-      </SectionContainer>
+      </div>
+
+      {/* SectionContainer at the bottom */}
+      <SectionContainer />
     </div>
   );
 }
