eb1f24076fb66991ebd8c12fb90b45c00f80d29a
Subash Baniya <154237809+sarkardocs@users.noreply.github.com>
2024-09-14 17:20:40 +0545

    Update not-found.tsx

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -10,29 +10,19 @@ import SectionContainer from '@/components/SectionContainer';
 
 export default function FourZeroFour() {
   return (
-    <div className="min-h-screen flex flex-col justify-between">
-      {/* Main Content */}
-      <div>
-        <div className="space-x-2 pt-6 pb-8 md:space-y-5">
-          <h1 className="text-6xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 md:border-r-2 md:px-6 md:text-8xl md:leading-14">
-            404
-          </h1>
-        </div>
-        <div className="max-w-md">
-          <p className="mb-4 text-xl font-bold leading-normal md:text-2xl">Page not found!</p>
-          <p className="mb-8">
-            Page under construction! But don't worry, you can find other content on our homepage.
-          </p>
-          <Link href="/">Back to homepage</Link>
-        </div>
+    <SectionContainer>
+      <div className="space-x-2 pt-6 pb-8 md:space-y-5">
+        <h1 className="text-6xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 md:border-r-2 md:px-6 md:text-8xl md:leading-14">
+          404
+        </h1>
       </div>
-
-      {/* SectionContainer at the bottom */}
-      <SectionContainer>
-        {/* Empty fragment, you can add content here if needed */}
-        <>
-        </>
-      </SectionContainer>
-    </div>
+      <div className="max-w-md">
+        <p className="mb-4 text-xl font-bold leading-normal md:text-2xl">Page not found!</p>
+        <p className="mb-8">
+          Page under construction! But dont worry, you can find other content on our homepage.
+        </p>
+        <Link href="/">Back to homepage </Link>
+      </div>
+    </SectionContainer>
   );
 }
